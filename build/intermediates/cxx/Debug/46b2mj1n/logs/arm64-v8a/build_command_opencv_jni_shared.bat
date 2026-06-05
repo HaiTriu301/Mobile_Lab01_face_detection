f19519bd1c5756131b2422b44a9e25861bca3b93
Hai Trieu <haitriuwork30@gmail.com>
2026-06-05 11:19:10 +0700

    push

---
+++ build/intermediates/cxx/Debug/46b2mj1n/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"D:\\AppCode\\AndroidStudio\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Study\\HK6\\Mobile\\opencv-4.5.2-android-sdk\\OpenCV-android-sdk\\sdk\\.cxx\\Debug\\46b2mj1n\\arm64-v8a" ^
+  opencv_jni_shared
